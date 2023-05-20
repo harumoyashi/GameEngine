b1475a2fc40c816464bb6b76ed659e43983f8544
晴彦 相川 <k021g1126@g.neec.ac.jp>
2023-05-20 13:53:30 +0900

    エラー消えないよ～

--- Resources/shaders/SpriteVS.hlsl
+++ Resources/shaders/SpriteVS.hlsl
@@ -3,8 +3,8 @@
 VSOutput main(float3 pos:POSITION, float2 uv : TEXCOORD)
 {
 	VSOutput output;	//ピクセルシェーダーに渡す値
-    float4 p = (pos, 1);
-	output.svpos = mul(mat, p);	//座標に行列を乗算
+    float4 p = mul(mat, float4(pos, 1)); //座標に行列を乗算
+    output.svpos = float3(p.x,p.y,p.z);
 	output.uv = uv;
 	return output;
 }
