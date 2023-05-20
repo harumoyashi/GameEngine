c0e73819ff7aef16cfd25fd26b5f0e18960ed9ce
晴彦 相川 <k021g1126@g.neec.ac.jp>
2023-05-20 13:34:31 +0900

    ん

--- Resources/shaders/SpritePS.hlsl
+++ Resources/shaders/SpritePS.hlsl
@@ -3,9 +3,7 @@
 Texture2D<float4>tex : register(t0);	//0番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);	//0番スロットに設定されたサンプラー
 
-float3 main(VSOutput input) : SV_TARGET
+float4 main(VSOutput input) : SV_TARGET
 {
-    float4 col = (tex.Sample(smp, input.uv)) * color; //定数バッファに送った色変えると反映される
-    return float3(col.x, col.y, col.z);
-
+	return float4(tex.Sample(smp,input.uv)) * color;	//定数バッファに送った色変えると反映される
 }
