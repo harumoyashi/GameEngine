a8b5741724e7d7fc40a1ed26e6349f0bcae0d7b9
晴彦 相川 <k021g1126@g.neec.ac.jp>
2023-06-29 04:10:38 +0900

    こみっとせい

--- Resources/shaders/ObjPS.hlsl
+++ Resources/shaders/ObjPS.hlsl
@@ -9,7 +9,7 @@ struct PSOutput
     float4 target1 : SV_TARGET1;
 };
 
-float4 main(VSOutput input) : SV_TARGET
+PSOutput main(VSOutput input) : SV_TARGET
 {
     PSOutput output;
     
@@ -131,6 +131,6 @@ float4 main(VSOutput input) : SV_TARGET
     float4 color = shadecolor * texcolor * m_color;
     output.target0 = color;
     output.target1 = color;
-    return color;
+    return output;
     //return shadecolor * texcolor * m_color;
 }
